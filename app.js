1334c1a9dc1084fba29ad62bb06de37dbda84686
salamisodikiolawale <salamisodikiolawale@gmail.com>
2022-10-29 02:17:00 +0200

    update app file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (request, response) => {
     response.status(200).json({
-        msg:'Hello Backend AWS EC2 DOCKER'
+        msg:'Hello Backend AWS EC2 DOCKER back_express_aws_ec2_docker'
     })
 })
 
